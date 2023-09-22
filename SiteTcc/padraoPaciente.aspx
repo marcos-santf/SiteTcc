151764becf11c97044ea1173a43dfd63eddf1ffd
Marcos Antonio <Marcos_9558@hotmail.com>
2023-09-22 00:20:22 -0300

    Ajuste cadastro paciente

--- SiteTcc/padraoPaciente.aspx
+++ SiteTcc/padraoPaciente.aspx
@@ -13,19 +13,23 @@
         <h1>Triagem Rápida: Agilizador de Atendimento de Saúde</h1>
     </header>
 
-    <section>
-        <h2>Informações do Usuário</h2>
-        <form id="userForm" runat="server">
+    <section id="pnPaciente" runat="server" visible="true">
+        <h2>Informações do Paciente</h2>
+        <form id="userForm" runat="server" >
             <p><h4>Nome Completo: </h4><input type="text" id="userName" onblur="restoreDefaultText(this)" runat="server" /></p>
             <p><h4>CPF: </h4><input type="text" id="userCpf" onblur="restoreDefaultText(this)" runat="server" oninput="this.value = this.value.replace(/\D/g, '')" /></p>
-            <p><h4>Data de Nascimento: </h4><input type="text" id="userDateOfBirth" runat="server" /></p>
+            <p><h4>RG: </h4><input type="text" id="userRg" onblur="restoreDefaultText(this)" runat="server" /></p>
+            <p><h4>Data de Nascimento: </h4><input type="date" id="userDateOfBirth" runat="server" /><input type="text" id="Date" runat="server" visible="false"/></p>
             <p><h4>Responsável: </h4><input type="text" id="userResponsavel" onblur="restoreDefaultText(this)" runat="server" /></p>
             <p><h4>Telefone: </h4><input type="text" id="userPhone" runat="server" /></p>
             <p><h4>E-mail: </h4><input type="text" id="userEmail"  onblur="restoreDefaultText(this)" runat="server" /></p>
+            <section id="pnSenha" runat="server">
             <p><h4>Senha: </h4><input type="password" id="userSenha"  onblur="restoreDefaultText(this)" runat="server" /></p>
             <p><h4>Confirme a senha: </h4><input type="password" id="userConfSenha"  onblur="restoreDefaultText(this)" runat="server" /></p>
-            <asp:Button ID="submitButton" runat="server" OnClick="submitButton_Click" Text="Enviar Dados" />
-            <%--<button class="action-button" onclick="submitButton_Click">Enviar Dados</button>--%>
+        <p><h4>Lembrete Senha: </h4><input type="text" id="userLembreteSenha" onblur="restoreDefaultText(this)" runat="server" /></p>
+            </section>
+            <asp:Button ID="submitButton" runat="server" Text="Enviar Dados" OnClick="submitButton_Click" CssClass="action-button" />
+            <%--<input  id="errorPopupMessage" style="font-size: 15px; text-align: center; color: red;" runat="server" visible="false" />--%>
         </form>
     </section>
 
--- SiteTcc/padraoPaciente.aspx.cs
+++ SiteTcc/padraoPaciente.aspx.cs
@@ -26,17 +26,73 @@ namespace SiteTCC
         }
         protected void submitButton_Click(object sender, EventArgs e)
         {
+            clsValidaDados validaDados = new clsValidaDados();
+
             string nomeCompleto = userName.Value; 
             string cpf = userCpf.Value;
+            string rg = userRg.Value;
             string dataNascimento = userDateOfBirth.Value;
             string responsavel = userResponsavel.Value;
             string telefone = userPhone.Value; 
             string email = userEmail.Value;
             string senha = userSenha.Value;
             string confirmaSenha = userConfSenha.Value;
+            string lembreteSenha = userLembreteSenha.Value;
+
+            if (dataNascimento != string.Empty)
+            {
+                TimeSpan idade = DateTime.Now - Convert.ToDateTime(dataNascimento);
+                if (idade.TotalDays < 18 * 365)
+                {
+                    validaDados.ClientMessage(this, "Favor, informar o Responsável.");
+                    return;
+                }
+            }
+
+            if (nomeCompleto == string.Empty)
+            {
+                validaDados.ClientMessage(this, "Favor, informar o Nome.");
+                return;
+            }
+            else if (cpf == string.Empty)
+            {
+                validaDados.ClientMessage(this, "Favor, informar o CPF.");
+                return;
+            }
+            else if (rg == string.Empty)
+            {
+                validaDados.ClientMessage(this, "Favor, informar o RG.");
+                return;
+            }
+            else if (dataNascimento == string.Empty)
+            {
+                validaDados.ClientMessage(this, "Favor, informar a Data de Nascimento.");
+                return;
+            }
+            else if (senha == string.Empty)
+            {
+                validaDados.ClientMessage(this, "Favor, informar a Senha.");
+                return;
+            }
+            else if (lembreteSenha == string.Empty)
+            {
+                validaDados.ClientMessage(this, "Favor, informar o Lembrete para Senha.");
+                return;
+            }
+            else if (senha != confirmaSenha)
+            {
+                validaDados.ClientMessage(this, "Senhas diferentes.");
+                return;
+            }
+            else
+            {
+                EnviarDadosParaBanco(nomeCompleto, cpf, rg, dataNascimento, responsavel, telefone, email, senha, lembreteSenha);
+            }
         }
         private void CarregaControle()
         {
+            userDateOfBirth.Visible = false;
+            Date.Visible = true;
             string Param2 = string.Empty;
             int CodigoUsuario = int.MinValue;
 
@@ -48,14 +104,67 @@ namespace SiteTCC
 
             userName.Value = (string)ds.Tables[0].Rows[0]["ds_nome"];
             userCpf.Value = (string)ds.Tables[0].Rows[0]["ds_cpf"];
-            userPhone.Value = (string)ds.Tables[0].Rows[0]["ds_telefone"];
+            userRg.Value = (string)ds.Tables[0].Rows[0]["ds_rg"];
             DateTime dataNascimento = (DateTime)ds.Tables[0].Rows[0]["dt_nascimento"];
-            userDateOfBirth.Value = dataNascimento.ToString("dd/MM/yyyy");
+            Date.Value = dataNascimento.ToString("dd/MM/yyyy");
+            userResponsavel.Value = (string)ds.Tables[0].Rows[0]["ds_responsavel"];
+            userPhone.Value = (string)ds.Tables[0].Rows[0]["ds_telefone"];
+            userEmail.Value = (string)ds.Tables[0].Rows[0]["ds_email"];
 
+            if(userName.Value != string.Empty)
             userName.Disabled = true;
+            if (userCpf.Value != string.Empty)
             userCpf.Disabled = true;
+            if (Date.Value != string.Empty)
+                Date.Disabled = true;
+            if (userPhone.Value != string.Empty)
             userPhone.Disabled = true;
-            userDateOfBirth.Disabled = true;
+            if (userEmail.Value != string.Empty)
+            userEmail.Disabled = true;
+
+            pnSenha.Visible = false;
+        }
+
+        private void EnviarDadosParaBanco(string nomeCompleto, string cpf, string rg, string dataNascimento,string responsavel,string telefone,string email,string senha, string lembreteSenha)
+        {
+            clsValidaDados validaDados = new clsValidaDados();
+            string Password = string.Empty;
+
+            try
+            {
+                Password = clsCriptografia.Encrypt(senha, "Eita#$%Nois##", true);
+
+                // Chama a stored procedure para inserir os dados no banco de dados
+                using (SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["MODELOConnectionString"].ConnectionString))
+                {
+                    // Abre a conexão
+                    connection.Open();
+
+                    // Criar o comando para a stored procedure
+                    using (SqlCommand cmd = new SqlCommand("pr_inclui_paciente", connection))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@ds_nome", nomeCompleto);
+                        cmd.Parameters.AddWithValue("@ds_rg", rg);
+                        cmd.Parameters.AddWithValue("@ds_cpf", cpf);
+                        cmd.Parameters.AddWithValue("@ds_telefone", telefone);
+                        cmd.Parameters.AddWithValue("@dt_nascimento", dataNascimento);
+                        cmd.Parameters.AddWithValue("@ds_responsavel", responsavel);
+                        cmd.Parameters.AddWithValue("@ds_email", email);
+                        cmd.Parameters.AddWithValue("@ds_senha", Password);
+                        cmd.Parameters.AddWithValue("@ds_lembrete_senha", lembreteSenha);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                Response.StatusCode = 200;
+                Response.Write("Dados inseridos com sucesso.");
+                Response.Redirect("padraoLogin.aspx");
+            }
+            catch (Exception ex)
+            {
+                validaDados.ClientMessage(this, ex.Message);
+                return;
+            }
         }
     }
 }
