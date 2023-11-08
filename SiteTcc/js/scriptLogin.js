58b1f85001fd92ccee36c2236dfd365d04d5f749
Marcos Antonio <marcos.santf@gmail.com>
2023-11-08 15:59:31 -0300

    Ajuste JS login

--- SiteTcc/js/scriptLogin.js
+++ SiteTcc/js/scriptLogin.js
@@ -4,13 +4,11 @@
 document.getElementById('cadastrar').addEventListener('click', redirecionarParaCadastro);
 
 function validaDadosAcesso() {
-    debugger
+    debugger;
     var cpf = document.getElementById('cpf').value;
     var password = document.getElementById('password').value;
-    if (cpf != null) {
-        if (password != null) {
-            window.location.href = 'padraoLogin.aspx?Param1=' + cpf + '&Param2=' + password + '&Param3=0';
-        }
+    if (cpf.trim() !== "" && password.trim() !== "") {
+        window.location.href = 'padraoLogin.aspx?Param1=' + cpf + '&Param2=' + password + '&Param3=0';
     }
 }
 document.getElementById('submit').addEventListener('click', validaDadosAcesso);
